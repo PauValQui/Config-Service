d238b573810dbcd9005ca005c0fcb3757e3a5946
Paulavq <91205671+PauValQui@users.noreply.github.com>
2022-10-03 12:57:43 +0200

    ConfigServer

---
+++ src/test/java/com/nttdata/springcloudserver/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.springcloudserver.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
